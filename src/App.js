fc885673a3855f8f58d4531924d88f4efd23ce95
BalaChandraAkula <balachandraakula698@gmail.com>
2023-08-29 13:07:38 +0530

    L8 Landing Page Left

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/Common/Footer';
 import Header from './components/Common/Header';
+import MainComponet from './components/LandingPage/MainComponent';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <MainComponet/>
       {/* <Footer/> */}
     </div>
   );
---
+++ src/components/LandingPage/MainComponent/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+import Button from '../../Common/Button'
+
+function MainComponet() {
+  return (
+    <div className='flex-info'>
+        <div className='left-component'>
+            <h1 className='track-crypto-heading'>Track Crypto</h1>
+            <h1 className='real-time-heading'>Real Time.</h1>
+            <p className='info-text'>Track crypto through a public api in real time. Visit the dashboard to do so!</p>
+            <div className='btn-flex'>
+                <Button text={"Dashboard"}/>
+                <Button text={"Share"} outlined={true}/>
+            </div>
+        </div>
+        <div>phone</div>
+    </div>
+  )
+}
+
+export default MainComponet
